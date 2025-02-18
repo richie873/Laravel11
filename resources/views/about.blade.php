03e12c3155d5bb904a305d2f9cf9f14574979c9c
RichieStevanus <richie.stevanus12@gmail.com>
2025-02-18 19:13:48 +0700

    Menambahkan File Projek Laravel11

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/welcome">Welcome</a>
+    
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dignissimos ad sunt libero, dolorum vero explicabo. Pariatur eveniet blanditiis temporibus reiciendis sint? Aperiam, nobis nostrum mollitia fugiat repellat consequuntur autem?</p>
+</body>
+</html>

6dffba74018396cfa658d4ea898427c24e0ffe0c
RichieStevanus <richie.stevanus12@gmail.com>
2025-03-05 21:30:54 +0700

    Membuat class didalam Routes Web

--- routes/web.php
+++ routes/web.php
@@ -3,56 +3,41 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+class Post {
+    public static function allArticle() {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Richie Stevanus',
+                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit officiis culpa 
+                voluptatibus dolorem quas quae quaerat itaque architecto eveniet totam, soluta nesciunt accusantium 
+                suscipit laudantium? Nobis eveniet beatae ad autem!'
+            ],
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Richie Stevanus',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad, vel accusantium 
+                repellendus cupiditate veniam molestiae. Sint eius ex, id porro veritatis impedit soluta, corporis 
+                minima molestias eveniet consectetur et.'
+            ],
+        ];
+    }
+}
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-    [
-        'id' => 1,
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1',
-        'author' => 'Richie Stevanus',
-        'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit officiis culpa 
-        voluptatibus dolorem quas quae quaerat itaque architecto eveniet totam, soluta nesciunt accusantium 
-        suscipit laudantium? Nobis eveniet beatae ad autem!'
-    ],
-    [
-        'id' => 1,
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2',
-        'author' => 'Richie Stevanus',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad, vel accusantium 
-        repellendus cupiditate veniam molestiae. Sint eius ex, id porro veritatis impedit soluta, corporis 
-        minima molestias eveniet consectetur et.'
-    ],
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::allArticle()]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-    [
-        'id' => 1,
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1',
-        'author' => 'Richie Stevanus',
-        'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit officiis culpa 
-        voluptatibus dolorem quas quae quaerat itaque architecto eveniet totam, soluta nesciunt accusantium 
-        suscipit laudantium? Nobis eveniet beatae ad autem!'
-    ],
-    [
-        'id' => 1,
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2',
-        'author' => 'Richie Stevanus',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad, vel accusantium 
-        repellendus cupiditate veniam molestiae. Sint eius ex, id porro veritatis impedit soluta, corporis 
-        minima molestias eveniet consectetur et.'
-    ],
-];
-
-    $post = arr::first($posts, function ($post) use ($slug) {
+    $post = arr::first(Post::allArticle(), function ($post) use ($slug) {
         return $post ['slug'] == $slug;
     });
 

a4cb5307d6493912b52a87361338c02e54628939
RichieStevanus <richie.stevanus12@gmail.com>
2025-03-06 19:07:21 +0700

    Memindahkan Class kedalam Model dan menambahkan Halaman 404

---
+++ app/Models/Article.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Article {
+    public static function allArticle() {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Richie Stevanus',
+                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit officiis culpa 
+                voluptatibus dolorem quas quae quaerat itaque architecto eveniet totam, soluta nesciunt accusantium 
+                suscipit laudantium? Nobis eveniet beatae ad autem!'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Richie Stevanus',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad, vel accusantium 
+                repellendus cupiditate veniam molestiae. Sint eius ex, id porro veritatis impedit soluta, corporis 
+                minima molestias eveniet consectetur et.'
+            ],
+        ];
+    }
+
+    public static function find($slug):array {
+        // return arr::first(static::allArticle(), function ($post) use ($slug) {
+        //     return $post ['slug'] == $slug;
+        // });
+        // return view('post', ['title' => 'Single Post', 'post' => $post]);    
+
+        return arr::first(static::allArticle(), fn ($post) => $post['slug'] == $slug) ?? abort(404);    
+    }  
+}
--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,19 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
-
-class Post {
-    public static function allArticle() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Richie Stevanus',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit officiis culpa 
-                voluptatibus dolorem quas quae quaerat itaque architecto eveniet totam, soluta nesciunt accusantium 
-                suscipit laudantium? Nobis eveniet beatae ad autem!'
-            ],
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Richie Stevanus',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad, vel accusantium 
-                repellendus cupiditate veniam molestiae. Sint eius ex, id porro veritatis impedit soluta, corporis 
-                minima molestias eveniet consectetur et.'
-            ],
-        ];
-    }
-}
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::allArticle()]);
+    return view('posts', ['title' => 'Blog', 'posts' => Article::allArticle()]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $post = arr::first(Post::allArticle(), function ($post) use ($slug) {
-        return $post ['slug'] == $slug;
-    });
-
+    $post = Article::find(($slug));
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
